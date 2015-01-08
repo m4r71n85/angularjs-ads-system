1a8f3cd3915f533fc9b337feb89e3f1d72d9fe69
m4r71n85 <martin.tsekov.bg@gmail.com>
2015-01-08 12:43:42 +0200

    Finalize authInterceptor

--- Ads.Web/App/interceptors/authInterceptor.js
+++ Ads.Web/App/interceptors/authInterceptor.js
@@ -3,35 +3,33 @@
 
         'use strict';
 
-        var checkNeedAuthToken = function (config) {
-
-            if (config.url.toLowerCase().indexOf("api") > -1) {
-                return true;
-            }
-
-            return false;
-
-        };
-
-        var addAuthToken = function (config) {
-            if (!checkNeedAuthToken(config)) return;
-
-            config.headers = config.headers || {};
-            var authData = "1";//authHelper.getAuth();
-            if (authData) {
-                config.headers.Authorization = 'Bearer ' + 1;//authData.token;
-            }
-        };
-
         var request = function (config) {
             addAuthToken(config);
-
             return config;
         };
 
+        var addAuthToken = function (config) {
+            if (!checkNeedAuthToken(config)) {
+                return;
+            }
+
+            config.headers = config.headers || {};
+            var authData = authSessionHelper.getToken();
+            if (authData) {
+                config.headers.Authorization = 'Bearer ' + authData;
+            }
+        };
+
+        var checkNeedAuthToken = function (config) {
+            if (config.url.toLowerCase().indexOf("api") > -1) {
+                return true;
+            }
+            return false;
+        };
+
+
         var responseError = function (rejection) {
             if (rejection.status === 403 || rejection.status === 401) {
-                //authHelper.clearAuth();
                 $location.url('/account/login');
             }
             return $q.reject(rejection);
