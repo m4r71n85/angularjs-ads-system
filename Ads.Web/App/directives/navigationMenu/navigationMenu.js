51e71f2d61aa442f30305801f53bd4d4fab0dd48
m4r71n85 <martin.tsekov.bg@gmail.com>
2015-01-11 12:44:36 +0200

    manually update nav menu on login/logout

--- Ads.Web/App/directives/navigationMenu/navigationMenu.js
+++ Ads.Web/App/directives/navigationMenu/navigationMenu.js
@@ -12,24 +12,29 @@ app.directive('navigationMenu',
                 $scope.isLoggedIn = authSessionHelper.isLoggedIn();
                 $scope.isAdmin = authSessionHelper.isAdmin();
 
-                if ($scope.isLoggedIn && $scope.isAdmin) {
-                    $scope.navigationMenuItems = [
-                        { title: "Ads", sref: "adminHome", authenticated: true },
-                        { title: "Users", sref: "adminUsers", authenticated: true },
-                        { title: "Categories", sref: "adminCategories", authenticated: true },
-                        { title: "Town", sref: "adminTowns", authenticated: true }
-                    ];
-                } else {
-                    $scope.navigationMenuItems = [
-                        { title: "Home", sref: "home" },
-                        { title: "My Ads", sref: "userAds", authenticated: true },
-                        { title: "Publish New Ad", sref: "publish", authenticated: true },
-                        { title: "Edit Profile", sref: "userProfile", authenticated: true }
-                    ];
+                function updateNavigation() {
+                    if ($scope.isLoggedIn && $scope.isAdmin) {
+                        $scope.navigationMenuItems = [
+                            { title: "Ads", sref: "adminHome", authenticated: true },
+                            { title: "Users", sref: "adminUsers", authenticated: true },
+                            { title: "Categories", sref: "adminCategories", authenticated: true },
+                            { title: "Town", sref: "adminTowns", authenticated: true }
+                        ];
+                    } else {
+                        $scope.navigationMenuItems = [
+                            { title: "Home", sref: "home" },
+                            { title: "My Ads", sref: "userAds", authenticated: true },
+                            { title: "Publish New Ad", sref: "publish", authenticated: true },
+                            { title: "Edit Profile", sref: "userProfile", authenticated: true }
+                        ];
+                    }
                 }
+                updateNavigation();
+
                 $scope.$on('authState', function () {
                     $scope.isLoggedIn = authSessionHelper.isLoggedIn();
                     $scope.isAdmin = authSessionHelper.isAdmin();
+                    updateNavigation();
                 });
             }
         ]
